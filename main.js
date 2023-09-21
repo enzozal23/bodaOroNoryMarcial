b3b0ac3e5d6ab61eb2019cc39d657289c35bba94
enzozal23 <enzo.z.1.ez@gmail.com>
2023-09-20 22:56:36 -0300

    celebracionOff

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let sound = new Audio("./maluma.mp3");
-let sound2 = new Audio("./malumaOgg.ogg");
+let sound2 = new Audio("../malumaOgg.ogg");
 let sound3 = new Audio("./malumaOpus.opus");
 const botonCelebracion = document.querySelector(".celebracion");
 const botonCeremonia = document.querySelector(".ceremonia");
@@ -8,10 +8,10 @@ const contenedorDatos2 = document.querySelector(".fechaLugar2");
 const botonCerrar = document.querySelector(".cerrar");
 const botonCerrar2 = document.querySelector(".cerrar2");
 
-botonCelebracion.addEventListener("click", (e) => {
-  e.preventDefault();
-  contenedorDatos.classList.add("activo");
-});
+// botonCelebracion.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   contenedorDatos.classList.add("activo");
+// });
 
 botonCerrar.addEventListener("click", (e) => {
   e.preventDefault();
@@ -21,9 +21,7 @@ botonCerrar.addEventListener("click", (e) => {
 botonCeremonia.addEventListener("click", (e) => {
   e.preventDefault();
   contenedorDatos2.classList.add("active");
-  sound.play;
-  sound2.play;
-  sound3.play;
+ 
 });
 console.log(contenedorDatos2);
 
@@ -31,11 +29,3 @@ botonCerrar2.addEventListener("click", (e) => {
   e.preventDefault();
   contenedorDatos2.classList.remove("active");
 });
-
-function playAudio(params) {
-  sound.play;
-  sound2.play;
-  sound3.play;
-}
-
-playAudio();
