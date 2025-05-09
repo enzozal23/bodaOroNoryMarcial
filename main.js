e6ce7b3fad5d47d44f88811c17964b7e41eee2ca
ENZO ZALAZAR <128516339+enzozal23@users.noreply.github.com>
2025-05-08 21:41:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,13 @@
-// let sound = new Audio("./maluma.mp3");
-// let sound2 = new Audio("../malumaOgg.ogg");
-// let sound3 = new Audio("./malumaOpus.opus");
-const botonCelebracion = document.querySelector(".celebracion");
-const botonCeremonia = document.querySelector(".ceremonia");
-const botonDress = document.querySelector(".dressCode");
-const contenedorDatos = document.querySelector(".fechaLugar");
-const contenedorDatos2 = document.querySelector(".fechaLugar2");
-const contenedorDatos3 = document.querySelector(".dressCode-container");
-const botonCerrar = document.querySelector(".cerrar");
-const botonCerrar2 = document.querySelector(".cerrar2");
-const botonCerrar3 = document.querySelector(".cerrar3");
-const musica = document.querySelector("#musica");
-console.log(musica);
-const botonConfirmar = document.querySelector(".whapp");
-
-
-
-
-botonCelebracion.addEventListener("click", (e) => {
-  e.preventDefault();
-  contenedorDatos.classList.add("activo");
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('openMapBtn').onclick = function() {
+    document.getElementById('mapModal').style.display = 'flex';
+  };
+  document.getElementById('closeMapBtn').onclick = function() {
+    document.getElementById('mapModal').style.display = 'none';
+  };
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('mapModal')) {
+      document.getElementById('mapModal').style.display = 'none';
+    }
+  };
 });
-
-botonCerrar.addEventListener("click", (e) => {
-  e.preventDefault();
-  contenedorDatos.classList.remove("activo");
-});
-
-botonCeremonia.addEventListener("click", (e) => {
-  e.preventDefault();
-  contenedorDatos2.classList.add("activo");
-});
-console.log(contenedorDatos2);
-
-botonCerrar2.addEventListener("click", (e) => {
-  e.preventDefault();
-  contenedorDatos2.classList.remove("activo");
-});
-
-botonDress.addEventListener("click", (e) => {
-  e.preventDefault();
-  contenedorDatos3.classList.add("activo");
-});
-botonCerrar3.addEventListener("click", (e) => {
-  e.preventDefault();
-  contenedorDatos3.classList.remove("activo");
-});
-
